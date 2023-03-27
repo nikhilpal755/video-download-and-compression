bc23582ffd9627cccb4b266cf5b3bac34b8d7db3
Nikhil Pal <nikhil16175@gmail.com>
2023-03-27 15:14:13 +0530

    applying changes

--- index.js
+++ index.js
@@ -5,42 +5,61 @@ import ffmpeg from 'ffmpeg-static'
 import { spawn } from 'child_process';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
+// FfM3VPj7a9o
 app.get('/downloadYoutubeVideo', async (req, res) => {
   try {
-    const videoUrl = 'https://www.youtube.com/watch?v=FfM3VPj7a9o';
+    // const videoId = req.query.v;
+    const videoId = 'FfM3VPj7a9o';
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
     const videoInfo = await ytdl.getInfo(videoUrl);
-
-    const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
     const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+    const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
 
-    const videoFile = fs.createWriteStream('video.mp4');
+    const audioFilePath = 'audio.mp4';
+    const videoFilePath = 'video.mp4';
+    const mergedFilePath = 'merged.mp4';
+
+    const audioWriteStream = fs.createWriteStream(audioFilePath);
+    ytdl(videoUrl, { format: audioFormat })
+      .pipe(audioWriteStream);
+
+    const videoWriteStream = fs.createWriteStream(videoFilePath);
     ytdl(videoUrl, { format: videoFormat })
-      .pipe(videoFile)
-      .on('finish', () => {
-        console.log('Video download complete');
-        res.status(200).sendFile('video.mp4', { root: __dirname });
-      })
-      .on('error', (err) => {
-        console.error('Video download error:', err);
-        res.status(500).send(`Video download error: ${err.message}`);
+      .pipe(videoWriteStream);
+
+    audioWriteStream.on('finish', () => {
+      const command = `${ffmpeg} -i ${videoFilePath} -i ${audioFilePath} -c:v copy -c:a aac -b:a 128k -map 0:v:0 -map 1:a:0 ${mergedFilePath}`;
+
+      const ffmpegProcess = spawn(command, { shell: true });
+      ffmpegProcess.on('error', (err) => {
+        console.error('Video merging error:', err);
+        res.status(500).send(`Video merging error: ${err.message}`);
       });
+      ffmpegProcess.on('exit', () => {
+        console.log('Video merging complete');
+        // res.status(200).sendFile(mergedFilePath, { root: __dirname });
+      });
+    });
   } catch (err) {
-    console.error('Error downloading video and audio:', err);
-    res.status(500).send(`Error downloading video and audio: ${err.message}`);
+    console.error('Error downloading video:', err);
+    res.status(500).send(`Error downloading video: ${err.message}`);
   }
+
 });
 
 
 app.get('/compressVideo', (req, res) => {
   try {
-    const videoFilePath = 'video.mp4';
+    const videoFilePath = 'merged.mp4';
     const compressedVideoFilePath = 'compressed.mp4';
     const command = `${ffmpeg} -i ${videoFilePath} -c:v libx264 -crf 28 -c:a aac -s 1280x720 -error-resilient 1 ${compressedVideoFilePath}`;
 
     const ffmpegProcess = spawn(command, { shell: true });
+    ffmpegProcess.stderr.on('data', (data) => {
+      console.error('ffmpeg error:', data.toString());
+    });
     ffmpegProcess.on('error', (err) => {
       console.error('Video compression error:', err);
       res.status(500).send(`Video compression error: ${err.message}`);
